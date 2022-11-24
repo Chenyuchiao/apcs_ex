ce5799569fff0e8f58494235ae7c7ed540aa8a3a
Chenyuchiao <113594518+Chenyuchiao@users.noreply.github.com>
2022-11-24 19:12:38 +0800

    Create 10-2.cpp

---
+++ 10-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char s[256];
+    int n,i,ii;
+    scanf("%s",s);
+    printf("SS:%s\n",s);
+    for(i=0;i<n && s[i] !='\0';i++){
+        printf("%c ",s[i]);
+    }
+    printf("\n");
+    printf("last index = %d\n",i);
+    ii = i;
+    for(i=ii-1;i>=0;i--){
+        printf("%c ",s[i]);   
+    }
+    return 0;
+}
