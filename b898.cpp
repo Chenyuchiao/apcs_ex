ffb4d22df3364d791ae0e517ae2178cb244f4f7d
Chenyuchiao <113594518+Chenyuchiao@users.noreply.github.com>
2022-12-22 20:00:05 +0800

    Create b898.cpp

---
+++ b898.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a, b, c;
+printf("input numbers: ");
+//scanf("%d ,%d, %d", &a, &b, &c);
+scanf("%d", &a);
+scanf("%d", &b);
+scanf("%d", &c);
+//printf("a=%d,b=%d,c=%d",a, b, c);
+if(b*b + c*c == a*a){
+printf("%d ", a);
+}else if(a*a + c*c == b*b){
+printf("%d ", b);
+}else if(a*a + b*b == c*c){
+printf("%d ", c);
+}else{
+printf("input numbers: ");
+}
+return 0;
+}
