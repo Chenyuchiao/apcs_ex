40d393595d0c697722dd8aa57b8fb4f77e101a4f
Chenyuchiao <113594518+Chenyuchiao@users.noreply.github.com>
2023-01-02 22:07:18 +0800

    Update c299.cpp

--- c299.cpp
+++ c299.cpp
@@ -1,22 +1,20 @@
-#include <stdio.h>
+#include <iostream>
 using namespace std;
 int main(){
 int n, i, a, b, max, min;
 scanf("%d",&n);
-for(i=0;i<n;i++){
-scanf("%d",&a);
-scanf("%d",&b);
+for(i=1;i<n;i++){
+scanf("%d", &a);
+scanf("%d", &b);
 }if(a>b){
 max=a,min=b;
 }else{
 max=b,min=a;
 }
-if(max-min==n-1){
-printf("%d,%d",min,max,"yes");
+if(max-min == n-1){
+printf("%d %d yes",min,max);
 }else{
-printf("%d,%d",a,b,"no");
+printf("%d %d no",a,b);
 }
 return 0;
 }
-
-
