e78813f4680be73b45ba476aecb57b7477835822
Chenyuchiao <113594518+Chenyuchiao@users.noreply.github.com>
2023-01-02 20:01:58 +0800

    Update c299.cpp

--- c299.cpp
+++ c299.cpp
@@ -1,10 +1,20 @@
 #include <stdio.h>
 using namespace std;
 int main(){
-int n, i, a, b, c;
+int n, i, a, b, max, min;
 scanf("%d",&n);
 for(i=0;i<n;i++){
-printf("input number: ");
-return 0;
+scanf("%d",&a);
+scanf("%d",&b);
+if(a>b){
+max=a,min=b;
+}else{
+max=b,min=a;
+}}
+if(max-min==n-1){
+printf("%d,%d",min,max,"yes");
+}else{
+printf("%d,%d",a,b,"no");
 }
+return 0;
 }
