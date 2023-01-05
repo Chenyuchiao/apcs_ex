5b67f09aaa116a879316797402f9f32e2526c8c3
Chenyuchiao <113594518+Chenyuchiao@users.noreply.github.com>
2023-01-05 18:31:25 +0800

    Create 13-1.cpp

---
+++ 13-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+struct Distance{
+    int feet;
+    float inch;
+};
+
+int main(){
+    struct Distance d1, d2;
+    
+    cout<<d2.inch<< " " <<d1.feet;
+    return 0;
+}
