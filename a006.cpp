47574dbfa4cdc008b3216cae29c163dd48860b04
Chenyuchiao <113594518+Chenyuchiao@users.noreply.github.com>
2023-01-08 20:35:19 +0800

    Create a006.cpp

---
+++ a006.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a, b, c, x, x1, x2;
+    scanf("%d %d %d", &a, &b, &c);
+    4*a*a*x*x+4*a*b*x==(-4)*a*c
+    x=
+    printf("Two different x1=%d,x2=%d", x1, x2);
+    return 0;
+}
