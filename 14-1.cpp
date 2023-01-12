ab783b26438970aba3e4faf860bc986b20fc9abf
Chenyuchiao <113594518+Chenyuchiao@users.noreply.github.com>
2023-01-12 18:58:32 +0800

    Update 14-1.cpp

--- 14-1.cpp
+++ 14-1.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-class Myclass {        // The class
-    public:            // Access specifier
-    int myNum;         // Attrioute (int variable)
-    string myString;   // Attrioute (string variable)
+class MyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;  // Attribute (string variable)
 };
 
 int main() {
-    Myclass myObj;    // Create an object of MyClass
-    
-    // Access attrioutes and set values
-    myObj.myNum = 15;
-    myObj.myString = "Some text";
-    
-    // Print values
-    cout << myObj.myNum << "\n";
-    cout << myObj.myString;
-    return 0;
+  MyClass myObj;  // Create an object of MyClass
+
+  // Access attributes and set values
+  myObj.myNum = 15;
+  myObj.myString = "Some text";
+
+  // Print values
+  cout << myObj.myNum << "\n";
+  cout << myObj.myString;
+  return 0;
 }
